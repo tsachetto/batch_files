dcac0189dc6c89561c5e1abca04a2d517183eb6c
thomaz sachetto <46068813+tsachetto@users.noreply.github.com>
2019-03-11 17:20:04 -0300

    Update Log_Folder.bat

--- Log_Folder.bat
+++ Log_Folder.bat
@@ -1,15 +1,11 @@
-
-'Código destinado a listar arquivos contidos em uma pasta definida pelo usuário, gerando um log com o resultado em .txt.
-
-=== copie arqui (código) =====
-
 @ECHO OFF 
+
+REM Código destinado a listar arquivos contidos em uma pasta definida pelo usuário, gerando um log com o resultado em .txt.
+
 
 CHCP 65001
 
 DIR /B "C:\" > C:\log_C.TXT
 
-=== fim código ===3
-
-Lembre-se de salvar com a extensão .bat
-Rodar no bloco de notas.
+Rem Lembre-se de salvar com a extensão .bat
+Rem Rodar no bloco de notas.
